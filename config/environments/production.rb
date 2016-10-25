832e86cd16f04a41e99a65af97034d79b1eebc0a
Vasanth <itsmevasanth@hotmail.com>
2016-10-25 08:44:25 +0530

    Added Logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,6 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
+
+  config.logger.level = Logger::DEBUG
 end
